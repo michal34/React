e3a9bddc9530f4990140693ef7d9b44694be39dc
Michał <86197538+michal34@users.noreply.github.com>
2022-05-19 20:27:33 +0200

    Add files via upload

---
+++ routes/src/components/header/Header.js
@@ -0,0 +1,8 @@
+const Header = (props) => {
+    return(
+        <div>
+            <h1>Header</h1>
+        </div>
+    )
+}
+export default Header
---
+++ routes/src/components/items/Items.js
@@ -0,0 +1,8 @@
+const Items = (props) => {
+    return(
+        <div>
+            <h1>Items</h1>
+        </div>
+    )
+}
+export default Items
